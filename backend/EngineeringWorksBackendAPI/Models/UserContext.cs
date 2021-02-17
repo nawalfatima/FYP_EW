d751d8dcc9f0fbb31e855d62f0887c1dd11cc8c4
Nawal <nawalfatima04@gmail.com>
2021-02-18 03:30:24 +0500

    backend and frontend

---
+++ backend/EngineeringWorksBackendAPI/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EngineeringWorks.Models
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext>options): base(options)
+        {
+
+        }
+        public DbSet<Admin> Admin { get; set; }
+
+        public DbSet<Client> Client { get; set; }
+    }
+}
